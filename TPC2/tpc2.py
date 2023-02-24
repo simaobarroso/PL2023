71f4974af23f403337595f255305b43dda5d1728
simaobarroso <simao.barroso@outlook.pt>
2023-02-24 21:38:34 +0000

    falta resolver bugs + duvidas, mas on e off funcemina

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -3,15 +3,19 @@
 
 # NOTA: NAO E PARA USAR REGEX (DISCORD - CURSO)
 
+# variavel global para definir se soma ou nao
+modo = 1 # 1 - on 0 - off
+
+# variaveis globais nem sepre boa ideia, ver alternativas
+
+
+on = ['on','ON','oN','On']
+off = ['OFF','off','Off','OFf','OfF','oFF','ofF','oFf']
+
+"""
 def trata(array):
     res = 0
-    for elem in array:
-        x = 0
-        try:
-            x = int(elem)
-        except:
-            pass    
-        res += x
+    #for elem in array:
     return res
 
 def exec1(): 
@@ -19,19 +23,56 @@ def exec1():
     res = 0
     for line in file: # tratamos de cada linha do ficheiro
         elem = line.split(" ") #queremos tratar de casa sequencia de caracteres de cada vez
-        res += trata (elem) # se forem numeos adicionamos ao resultado final
+        for a in elem:
+            
+        #res += trata (elem) # se forem numeos adicionamos ao resultado final
     return res
+"""
+
+# VER PERGUNTAS BUGADORES ! + DISCORD
+def trataString(a,res): # para resolver o caso do ```1 1=``` passo o res como parametro NAO DA PORQUE ISTO E PARA CADA STRING
+    global modo
+    r = 0
+    if a in on and modo==0:
+        modo = 1
+        #print("foi alterado o modo para off, qualquer cena depois de on soma")
+    elif (a in off) : 
+        modo = 0
+        #print("foi alterado o modo para off, qualquer cena depois de off nao soma")
+        return 0
+    elif modo == 0: return 0
+    str = "0"
+    i = 0
+    tam = len(a)    
+    for c in a:
+        if c=='=' : print(res)
+        if c.isdigit() : 
+            str += c    
+        else:
+            r += int(str)
+            str = "0"
+        i+=1
+        if i == tam: res+=int(str) # ineficiente? sim
+    return r
+
 
 
 def exec2(): 
+    global resultado
     dados = input("Coloque aqui os dados:")
+    res = 0
     elem = dados.split(" ")
-    res = trata(elem)
+    for a in elem:
+        res += trataString(a,res) # depois rever para o igual !!!!!!! e ter em atencao que on\n conta como on (ou qualquer combinacao assim)
+        # ver maneiras mais eficientes ! bigO percorrer uma vez
+    #res = trata(elem)
     return res
 
 def main():
-    print(exec1()) # le de um ficheiro de texto e soma todos os digitos
-    print(exec2())
+    #print(exec1()) # le de um ficheiro de texto e soma todos os digitos
+    print("Por default esta on")
+    exec2()
+    #print('a'.isdigit())
 
 if __name__=='__main__':
     main()
