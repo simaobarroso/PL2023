e33b5560bf2ff2e040363aa7e9d895223a7ca6b6
simaobarroso <simao.barroso@outlook.pt>
2023-03-02 00:04:13 +0000

    TPC2 : falta otimizar + ReadMe

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -1,86 +1,86 @@
 # TPC2 - UC de Processamento de linguagens
 # simaobarroso
 
-# NOTA: NAO E PARA USAR REGEX (DISCORD - CURSO)
+import sys
 
 # variavel global para definir se soma ou nao
 modo = 1 # 1 - on 0 - off
 resultado = 0
-# variaveis globais nem sepre boa ideia, ver alternativas
 
 
-on = ['on','ON','oN','On']
-off = ['OFF','off','Off','OFf','OfF','oFF','ofF','oFf']
 
-"""
-def trata(array):
-    res = 0
-    #for elem in array:
-    return res
+# none conta como on 
+# proffessor conta como off
+def checkOnOff(string): #verifica se a string tem um on off e =
+    res = -1
+    fase = 0
+    for char in string:
+        if char == 'o' or char == 'O':
+            fase = 1
+        if (char == 'n' or char == 'N') and fase ==1:
+            return 1
+        elif (char == 'f' or char == 'F') and fase == 1:
+            fase = 2
+        elif (char == 'f' or char == 'F') and fase == 2:
+            return 0 
+    return res            
 
-def exec1(): 
-    file = open("text.txt")
-    res = 0
-    for line in file: # tratamos de cada linha do ficheiro
-        elem = line.split(" ") #queremos tratar de casa sequencia de caracteres de cada vez
-        for a in elem:
-            
-        #res += trata (elem) # se forem numeos adicionamos ao resultado final
-    return res
-"""
-
-# VER PERGUNTAS BUGADORES ! + DISCORD
-def trataString(a,res): # para resolver o caso do ```1 1=``` passo o res como parametro NAO DA PORQUE ISTO E PARA CADA STRING
+# eficiencia nao, mas leitura facilitada
+def trataString(a):
     global modo
     global resultado
-    r = 0
-    if a in on and modo==0:
+    r = checkOnOff(a)
+    if r==1:
         modo = 1
-        #print("foi alterado o modo para off, qualquer cena depois de on soma")
-    elif (a in off) : 
+    elif (r==0) : 
         modo = 0
-        #print("foi alterado o modo para off, qualquer cena depois de off nao soma")
-        return 0
-    elif modo == 0: return 0
     str = "0"
     i = 0
     tam = len(a)    
     for c in a:
-        if c.isdigit() : 
+        if c.isdigit() and modo != 0 :
             str += c    
-        elif c != '=' and c.isdigit() == False :
+        elif c != '=' and c.isdigit() == False and modo != 0:
             resultado += int(str)
             str = "0"
         if c=='=': 
             resultado += int(str)
-            print(resultado)    
+            str = "0"
+            print(resultado)
         i+=1
-        if i == tam: resultado+=int(str) # ineficiente? sim
-        #print(str)
-    #resultado += r
-# TER EM CONSIDERACAO \n e \t e cenas assim (uma ou varias ocurrencias)
+        if i == tam and modo != 0: resultado+=int(str) # ineficiente? sim
 
+
+# Notas:
+# Como apenas contam sequencias de digitos para somar ignora-se o - nos numeros negativos
+# ou seja  -55 fica 55
+# isto e` facilmente resolvido com (mais uma) condicao na funcao acima.
+# uma vez que a func int do python tambem passa numeros negativos dai era so verficar que se fosse - adicionava-se a string str
+
+
+def exec1():
+    f = open("text.txt")
+    for line in f:
+        elem = line.split(" ")
+        for a in elem:
+            trataString(a)
 
 def exec2(): 
     global resultado
-    dados = input("Coloque aqui os dados:")
-    elem = dados.split(" ")
-    res = 0
-    for a in elem:
-        trataString(a,res) # vai escrever o resultado desta para variavel global
-        #resultado += trataString(a,res) # depois rever para o igual !!!!!!! e ter em atencao que on\n conta como on (ou qualquer combinacao assim)
-        # ver maneiras mais eficientes ! bigO percorrer uma vez
-    #res = trata(elem)
-    #return res
+    print("Coloque aqui os dados:")
+    while (True):
+        dados  = input()
+        elem = dados.split(" ")
+        for a in elem:
+            trataString(a)
 
 
 
-# FAZER EXERCICIO 1 !!!!!!
+
 def main():
-    #print(exec1()) # le de um ficheiro de texto e soma todos os digitos
-    print("Por default esta on")
-    exec2()
-    #print('a'.isdigit())
+    m = input("Quer ler do ficheiro text.txt (1) ou do terminal (2) : ")    
+    if m == '1' : exec1()
+    else : exec2()
 
 if __name__=='__main__':
     main()
