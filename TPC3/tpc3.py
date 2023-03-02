966fae042290a68f2c25042f681fab6df115035c
simaobarroso <simao.barroso@outlook.pt>
2023-03-02 12:04:21 +0000

    TPC3: INICIO

---
+++ TPC3/tpc3.py
@@ -0,0 +1,19 @@
+# TPC3: Processador de Pessoas listadas nos Róis de Confessados
+# simaobarroso
+
+import re # usar regular expressions
+
+# regex101
+
+
+
+
+
+"""
+def main():
+    
+
+if __name__=='__main__':
+    main()
+
+"""
