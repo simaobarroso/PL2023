55aa8e9d870c67de96427548ca96286889efb7e0
simaobarroso <simao.barroso@outlook.pt>
2023-03-03 21:57:45 +0000

    3 : exec 1 feito

--- TPC3/tpc3.py
+++ TPC3/tpc3.py
@@ -7,21 +7,62 @@ import re # usar regular expressions
 
 
 
+def exec1(dados):
+    res = dict()
+    regex = r"\d+"
+    for elem in dados:
+        data = elem['data']
+        #print(re.match(regex,data).group()) # ver ipynb aula teorica
+        data = re.match(regex,data).group()
+        #print(data)
+        if data in res.keys():
+            res[data] += 1
+        else :
+            res[data] = 1
+    return dict(sorted(res.items())) # nao era mais eficiente ir colocando organizado `a medida que vamos por no dicionario
+
+
+
+
+
 
 def trata(array):
     res = dict()
-    print(array)
-
-    if len(array) != 7 :
-        """
-        dict['pasta'] = array[0]
-        dict['data'] = array[1]
-        dict['nome'] = array[2]
-        dict['pai'] = array[3]
-        dict['mae'] = array[4]
-        dict['observacoes'] = array[5]
-        """
+    res['pasta'] = array[0] # pasta/processo
+    res['data'] = array[1]
+    res['nome'] = array[2]
+    res['pai'] = array[3]
+    res['mae'] = array[4]
+    res['observacoes'] = array[5]    
     return res
+
+# REUTILIZADA DO TPC1
+# a funcao que imprime a tabela dando um dicionario
+def printTable(distribuicao):
+    for key in distribuicao:
+        print("-----------------------------")
+        string = '|' # barra do lado esquerdo
+        comp = int((13-len(key))/2) # quantos espacos podemos por de modo a key ficar centralizado
+        for i in range(comp):
+            string += ' '
+        string += key # adicionamos a key
+        for i in range(comp):
+            string += ' '
+        string += "|" # colocar barro do meio (separacao entre a key e o valor do dict)
+        comp = 13-len(str(distribuicao[key])) # o mesmo do de cima, mas para o valor da key
+        if comp % 2 != 0: 
+            comp = int(comp/2)
+            comp1 = comp+1
+        else:
+            comp = int(comp/2)
+            comp1 = comp
+        for i in range(comp):
+            string += ' '
+        string += str(distribuicao[key])
+        for i in range(comp1):
+            string += ' '
+        print(string+"|") # barra do lado direito
+    print("-----------------------------")
 
 
 # Fazer uma lista de dicionarios ou um dicionarios de dicionarios ?
@@ -29,15 +70,22 @@ def trata(array):
 # ver ipynb (jupiter notebook) 
 
 def main():
-    print("Processing processos.txt ...")
-    data = list()
+    #print("Processing processos.txt ...")
+    dados = list()
     f = open('processos.txt')
     for line in f:
         campos = line.split("::")
-        data += trata(campos)
-        #print(campos)
-        #time.sleep(5)
-    print(data)    
+        if (len(campos)) == 7:
+            dados += [trata(campos)]
+    #print(dados[63])
+    alinea = input("Alinea do exercicio : ") 
+    if alinea == "ap":
+        print("-----------------------------")
+        print("|     Processos por ano     |")
+        print("-----------------------------")
+        printTable(exec1(dados))
+    elif alinea == "as":
+        print(exec1(dados))    
 
 
 
