7ed9a5a1f18eb9a9ebec957087570aadbb8be4d0
simaobarroso <simao.barroso@outlook.pt>
2023-03-04 00:58:39 +0000

    exercicio 2 mal

--- TPC3/tpc3.py
+++ TPC3/tpc3.py
@@ -6,7 +6,8 @@ import re # usar regular expressions
 # regex101 para validar
 
 
-
+# tambem da para fazer por splits nesta pergunta
+# splits por '-'
 def exec1(dados):
     res = dict()
     regex = r"\d+"
@@ -21,15 +22,83 @@ def exec1(dados):
             res[data] = 1
     return dict(sorted(res.items())) # nao era mais eficiente ir colocando organizado `a medida que vamos por no dicionario
 
+def aux(res,primeiroNome,ultimoNome):
+
+    #print(dict)
+    #time.sleep(5)
+
+    #print(res.keys())
+    if primeiroNome not in res.keys() and ultimoNome not in res.keys():
+        res[primeiroNome] = 1
+        res[ultimoNome] = 1
+    elif primeiroNome in res.keys() and ultimoNome not in res.keys() : 
+        res[primeiroNome] += 1
+        res[ultimoNome] = 1
+    elif primeiroNome not in res.keys() and ultimoNome in res.keys() : 
+        res[ultimoNome] += 1
+        res[primeiroNome] = 1
+    else:
+        res[primeiroNome] += 1
+        res[ultimoNome] += 1
+
+    
+    
+    #print(res)
+    #time.sleep(3)
+    return res
+    #return dict(sorted(res.items()))
+  
+
+
 
 def exec2(dados):
     # int(data) // 100 +1 # -> e` assim que se calcula o seculo
     # \w+$ # regex ultimo nome
-    # ^\w+ # regex ultimo nome
+    # ^\w+ # regex primeiro nome
     # (^\w+|\w+$) # regex primeiro e ultimo nome
+    # ou fazer split por espacos !!!!
+    res = dict()
+    for elem in dados :
+        data = elem['data']
+        data = re.match(r"\d+",data).group()
+        seculo = int(data) // 100 + 1
+        primeiroNome = re.match(r"^\w+",elem['nome']).group()
+        ultimoNome = re.search(r"\w+$",elem['nome']).group()
+        if seculo in res.keys():
+            res[seculo] = aux(res[seculo],primeiroNome,ultimoNome)
+        else:
+            res[seculo] = {primeiroNome : 1, ultimoNome : 1}
+        #print(res)
+        """
+        if seculo in res.keys() and primeiroNome in res[seculo].keys() and ultimoNome in res[seculo].keys():
+            res[seculo][primeiroNome] += 1
+            res[seculo][ultimoNome] += 1
+        elif primeiroNome in res[seculo].keys() and ultimoNome  not in res[seculo].keys():
+            res[seculo][primeiroNome] += 1
+            res[seculo] = {ultimoNome : 1}
+        elif primeiroNome not in res[seculo].keys() and ultimoNome in res[seculo].keys():
+            res[seculo] = {primeiroNome : 1}
+            res[seculo][ultimoNome] += 1
+        else : # caso de nao existir primeiro 
+            res[seculo] = {primeiroNome : 1}
+            res[seculo] = {ultimoNome : 1}
+        """
+    return dict(sorted(res.items()))
 
 
+    """
+        if data in res.keys():
+            res[seculo] += 
+        else :
+            res[seculo] = 
+    return dict(sorted(res.items())) 
+    """
 
+def exec3(dados):
+    pass 
+    # basta usar regex para apanhar tudo depois da virgurla (do campo das observacoes)
+    # regex 101 para confirmar 
+    # 
 
 def trata(array):
     res = dict()
@@ -81,8 +150,22 @@ def main():
     for line in f:
         campos = line.split("::")
         if (len(campos)) == 7:
+            # falta validar se esta repetido ou nao os campos !!!
+            # usar regex para validar campos (discord)
             dados += [trata(campos)]
     #print(dados[63])
+    #print(exec2(dados))
+    dass = exec2(dados)
+    i = 0
+
+    for elem in dass:
+        try:
+            i+= dass[elem]['Adao']             
+        except: 
+            pass    
+    print(i)        
+    
+    """
     alinea = input("Alinea do exercicio : ") 
     if alinea == "ap":
         print("-----------------------------")
@@ -91,7 +174,7 @@ def main():
         printTable(exec1(dados))
     elif alinea == "as":
         print(exec1(dados))    
-
+        """
 
 
 if __name__=='__main__':
@@ -111,5 +194,13 @@ Explicar no read.me
 +
 
 Fazer validacao dos dados !!!!
+
++
+
+githubs
+
++
+re.compile(r'^(?P<pasta>[0-9]+)::(?P<data>\d{4}-\d{2}-\d{2})::(?P<nome>[a-zA-Z ]+)::(?P<pai>[a-zA-Z ]+)?::(?P<mae>[a-zA-Z ,]+)?::(?P<observacoes>.+)[:]+$')
+
 
 """
