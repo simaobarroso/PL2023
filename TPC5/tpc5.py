1a47843d84b10326a16f4028d83fab2bff6da714
simaobarroso <simao.barroso@outlook.pt>
2023-03-21 22:26:01 +0000

    TPC5 PRONTO

--- TPC5/tpc5.py
+++ TPC5/tpc5.py
@@ -1,84 +1,91 @@
 # tpc5 - 
 # simaobarroso
 
-# utilizar ply? 
-# modulo re
-
-
-# ver outras maneiras + melhorar regex + ply + module re !!!
-
-# melhorar isto !
-
-# discord + aulas teoricas
 import re
 
-def calcTroco(saldo): #suponho que o saldo venha em numero inteiro
-    # saldo das moedas dado em centimos
-    res1 = ""
-    res2 = ""
-    e = int (saldo / 100) # esta incorreto (dividir troco em moedas?)
-    res1 += e +"e "
-    resto = saldo % 100
-    res1 += resto + "c"
-    while(resto != 0):
-        if resto>50:
-            res += "50c "
-            resto -= 50
-        elif resto > 20:
-            res += "20c "
-            resto -= 20
-        elif resto > 10:
-            res += "10c "
-            resto -= 10
-        elif resto > 10:
-            res += "10c "
-            resto -= 10
-        elif resto > 5:
-            res += "5c "
-            resto -= 5 
-        else:
-            res += str(resto) +"c "
-            resto -= resto    
+def somaCoins(lmoedas):
+    res = 0
+    lcoins = lmoedas.split(',')
+    for l in lcoins:
+        if e := re.fullmatch(r'(1|2)e',l):
+            res+= 100*int(e.group(1))
+        elif c:=re.fullmatch(r'(1|2|5|10|20|50)c',l) :
+            res+= int(c.group(1))
+        else: print("Moeda " + l + " no formato errado")
+    return res
 
-    return res1,res2
+
+def chamada(numero):
+    custo = 0
+    if re.match(r"^00\d+", numero):
+        custo += 150
+    elif re.fullmatch(r"(0{2})?\d{9}", numero):
+        if re.match(r"601", numero) or re.match(r"641", numero):
+            print("Esse número não é permitido neste telefone. Escreva um novo número!")
+        elif re.match(r"2", numero):
+            custo += 25
+        elif re.match(r"808", numero):
+            custo += 10
+        elif re.match(r"800", numero):
+            custo += 0
+        else:
+            print("O número para o qual ligou não está atribuído!")
+    else:
+        print("O número apresentado não apresenta um formato correto! Por favor insira um novo número!")
+
+    return custo
+
+def devolver(saldo):
+    euros = saldo // 100
+    centimos = saldo % 100
+    if euros == 0:
+        return "{}c".format(centimos)
+    elif centimos == 0:
+        return "{}e".format(euros)
+    else:
+        return "{}e{}c".format(euros, centimos)
 
 
 def main():
-    m
     mode = 0 
     # 0 -> desligado
     # 1 -> Ligado
-    # 2 -> ligado e vai receber um numero
-    # 3 -> ligado e vai receber moedas
     t = True
-    saldo = 0
+    saldo = 0 #saldo em centimos
     while(t):
         i = input("> ")
-        rgW = r'\w+' # Confirmar com testes, mas esta a funcionar
-        l = re.findall(rgW,i)
-        n = 0
-        for e in l:
-            # match (e):
-            if e == "LEVANTAR":
+        if re.fullmatch(r"\s*LEVANTAR\s*", i):
+            if mode:
+                print("maq: O telefone já está levantado. Insira moedas ou ligue para um número!")
+            else:
+                print("maq: Introduza Moedas.")
                 mode = 1
-                print("maq > Introduza moedas.")
-            elif e == "POUSAR":
-                mode = 0
-                troco = calcTroco(saldo)
-                print("maq > Troco = " + troco) # mudar isto
-                t = False # suponho que depois do pousar nao possa vir um levantar
-            elif e == "T":
-                mode = 2
-                pass
-            elif e == "MOEDA":
-                mode = 3
-            elif e == "ABORTAR":
-                pass
-            elif re.match(r"\d+",e) and mode == 2:      
-                # numero de telemovel 
-                mode = 1
+        elif (numero := re.fullmatch(r"\s*T=(\d+)\s*", i)) and mode == 1:
+            custo = chamada(numero.group(1))
+            if custo > saldo:
+                print("maq: Saldo Insuficiente! Por favor introduza mais moedas.")
+            else:
+                saldo -= custo
+                print("maq: \"saldo="+ devolver(saldo)+"\"") 
+        elif (lmoedas := re.fullmatch(r"\s*MOEDA\s([\w, ]*)\s*", i) )and mode == 1:
+            saldo += somaCoins(lmoedas.group(1))
+            print("maq: \"saldo="+ devolver(saldo)+"\"") 
+        elif re.fullmatch(r"\s*ABORTAR\s*", i) and mode == 1:
+            print("maq: \"troco="+devolver(saldo)+"; Obrigado!\"")
+            break
+        elif re.fullmatch(r"\s*POUSAR\s*", i) and mode == 1:
+            print("maq: \"troco="+devolver(saldo)+"; Obrigado!\"")
+            break
+        else:
+            print("maq: Operação Inválida")
+            #break
 
-            n+=1
 
 if __name__=="__main__":
     main()    
+
+
+"""
+Notas:
+Melhorar correcao de erros
+"""
